b5ae74f555a866c97420556fcb9e5575e95ba0f4
krishpranav <krisna.pranav@gmail.com>
2021-04-23 18:10:57 +0530

    added different colors

--- gorevshell.go
+++ gorevshell.go
@@ -41,22 +41,24 @@ func list(){
 		[]string{"shutdown", "Shutdown computer"},
         []string{"custom", "Use custom Bash and Powershell scripts"},
     }
-
-		actions_table := tablewriter.NewWriter(os.Stdout)
-		actions_table.SetAutoWrapText(false)
-		actions_table.SetHeader([]string{"NAME", "DESCRIPTION"})
-	actions_table.SetColumnColor(
-		tablewriter.Colors{tablewriter.FgGreenColor},
-		tablewriter.Colors{},
-	)
-
+	actions_table := tablewriter.NewWriter(os.Stdout)
+	actions_table.SetAutoWrapText(false)
+	actions_table.SetHeader([]string{"NAME", "DESCRIPTION"})
+    actions_table.SetColumnColor(
+        tablewriter.Colors{tablewriter.FgGreenColor},
+        tablewriter.Colors{}, 
+    )
 	for v := range actions_data {
 		actions_table.Append(actions_data[v])
 	}
-
 	fmt.Println("")
-	fmt.Println("[+] Payloads: ")
-
-	actions_table.Render()
-	fmt.Println("")
+	fmt.Println("[*] Payloads: ")
+    actions_table.Render()
+    fmt.Println("")
 }
+
+// colors
+var red = color.New(color.FgRed).SprintFunc()
+var green = color.New(color.FgGreen).SprintFunc()
+var cyan = color.New(color.FgBlue).SprintFunc()
+var bold = color.New(color.Bold).SprintFunc()
