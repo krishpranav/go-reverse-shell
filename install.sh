a8d96bf0ede704fc774b73a3f5b9855db00c7745
krishpranav <krisna.pranav@gmail.com>
2021-04-23 17:56:00 +0530

    Added go get values

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# go get values
+go get -u github.com/akamensky/argparse
+go get -u github.com/fatih/color
+go get -u github.com/olekukonko/tablewriter
+go get -u github.com/chzyer/readline
+go get -u github.com/common-nighthawk/go-figure
+go get -u github.com/gobuffalo/packr
